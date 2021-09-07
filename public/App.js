88718dac0d49d2917af69c50444ef4ef05653869
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-09-07 14:07:32 +0200

    Added event listener to the form, added task factory

--- public/App.js
+++ public/App.js
@@ -1,1 +1,19 @@
-console.log("test");
+// console.log("test");
+
+const form = document.querySelector(".add-task-form");
+const input = document.querySelector(".add-task-input");
+
+const taskFactory = (title) => {
+  const isDone = false;
+  return { title, isDone };
+};
+
+const addedTask = (event) => {
+  event.preventDefault();
+  let title = input.value;
+  console.log("submitted", "title is", title);
+  let newTask = taskFactory(title);
+  console.log("new task", newTask);
+};
+
+form.addEventListener("submit", addedTask);
