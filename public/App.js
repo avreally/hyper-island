886967fdb3aebf11f2dd2d7f17d1cef0937ad7ee
Valeria-Shadrina <waleria.shadrina@gmail.com>
2021-09-10 14:31:54 +0200

    Implemented adding a task to the DOM

--- public/App.js
+++ public/App.js
@@ -1,19 +1,46 @@
-// console.log("test");
+// import { format } from "date-fns";
 
 const form = document.querySelector(".add-task-form");
 const input = document.querySelector(".add-task-input");
+const taskContainer = document.getElementById("todo-container");
 
 const taskFactory = (title) => {
   const isDone = false;
+  // const creationDate = format(Date.now());
   return { title, isDone };
 };
 
-const addedTask = (event) => {
-  event.preventDefault();
-  let title = input.value;
-  console.log("submitted", "title is", title);
-  let newTask = taskFactory(title);
-  console.log("new task", newTask);
+const tasksList = [];
+
+const appendTaskToDOM = (task) => {
+  let div = document.createElement("div");
+
+  let label = document.createElement("label");
+  label.textContent = task.title;
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+
+  label.prepend(checkbox);
+
+  div.append(label);
+  div.classList.add("todo-item");
+
+  taskContainer.append(div);
 };
 
-form.addEventListener("submit", addedTask);
+const addTask = (event) => {
+  event.preventDefault();
+  let title = input.value;
+
+  let newTask = taskFactory(title);
+  console.log("new task", newTask);
+
+  tasksList.push(newTask);
+
+  appendTaskToDOM(newTask);
+  console.log("tasks list", tasksList);
+  form.reset();
+};
+
+form.addEventListener("submit", addTask);
